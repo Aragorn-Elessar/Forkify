71ccf3079b7e29f7b0c9e51ad180e58972e49ec6
Aragorn Elessar <mahmoud_gadallah@outlook.com>
2023-11-15 16:56:35 +0200

    Add guard clause for showRecipe function

--- src/js/controller.js
+++ src/js/controller.js
@@ -34,6 +34,9 @@ const showRecipe = async function () {
     const id = window.location.hash.slice(1);
     console.log(id);
 
+    // Guard clause
+    if (!id) return;
+
     // 1) Load recipe
     renderSpinner(recipeContainer);
 
