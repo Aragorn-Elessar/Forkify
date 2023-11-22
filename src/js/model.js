4bc6041a394143a38228d00aad955cb0221a9114
Aragorn Elessar <mahmoud_gadallah@outlook.com>
2023-11-22 08:19:00 +0200

    Fix: pagination doesn't reset on new search

--- src/js/model.js
+++ src/js/model.js
@@ -38,6 +38,7 @@ export const loadSearchResults = async function (query) {
     state.search.query = query;
 
     const data = await getJSON(`${API_URL}?search=${query}`);
+    console.log(data);
 
     state.search.results = data.data.recipes.map(rec => {
       return {
@@ -47,7 +48,7 @@ export const loadSearchResults = async function (query) {
         image: rec.image_url,
       };
     });
-    console.log(state.search.results);
+    state.search.page = 1;
   } catch (err) {
     console.error(`${err} 💥💥💥`);
     throw err;
