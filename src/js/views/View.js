e90dfa389ef4390548c05e5abd4dec51a3c17fe1
Aragorn Elessar <mahmoud_gadallah@outlook.com>
2023-11-23 07:12:17 +0200

    refactor: Create a previewView child class to encapsulate the shared markup in results/bookmarks view

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -3,13 +3,17 @@ import icons from 'url:../../img/icons.svg';
 export default class View {
   _data;
 
-  render(data) {
+  render(data, render = true) {
     // if no data || data is an array && is empty
     if (!data || (Array.isArray(data) && data.length === 0))
       return this.renderError();
 
     this._data = data;
     const markup = this._generateMarkup();
+
+    // Instead of rendering markup to the DOM, render method returned to markup as a string.
+    if (!render) return markup;
+
     this._clear();
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
   }
