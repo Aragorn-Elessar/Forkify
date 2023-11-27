c735a7342b5cc392fd6fe2c2b1f31771ee5e8853
Aragorn Elessar <mahmoud_gadallah@outlook.com>
2023-11-27 07:15:00 +0200

    feat: Format new uploaded recipe forum data

--- src/js/controller.js
+++ src/js/controller.js
@@ -94,10 +94,14 @@ const controlBookmarks = function () {
   bookmarksView.render(model.state.bookmarks);
 };
 
-const controlAddRecipe = function (newRecipe) {
-  console.log(newRecipe);
-
-  // Upload new recipe data
+const controlAddRecipe = async function (newRecipe) {
+  try {
+    // Upload new recipe data
+    await model.uploadRecipe(newRecipe);
+  } catch (err) {
+    console.error('💥', err);
+    addRecipeView.renderError(err.message);
+  }
 };
 
 const init = function () {
