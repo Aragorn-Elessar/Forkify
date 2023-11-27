ff654b6711396a207864a01d9f57aedd17467fca
Aragorn Elessar <mahmoud_gadallah@outlook.com>
2023-11-27 11:25:49 +0200

    feat: Show new recipe success msg and close form window

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,4 +1,5 @@
 import * as model from './model.js';
+import { MODAL_CLOSE_SEC } from './config.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
@@ -98,6 +99,15 @@ const controlAddRecipe = async function (newRecipe) {
   try {
     // Upload new recipe data
     await model.uploadRecipe(newRecipe);
+    console.log(model.state.recipe);
+
+    // Success message
+    addRecipeView.renderMessage();
+
+    // Close form window
+    setTimeout(function () {
+      addRecipeView.toggleWindow();
+    }, MODAL_CLOSE_SEC * 1000);
   } catch (err) {
     console.error('💥', err);
     addRecipeView.renderError(err.message);
